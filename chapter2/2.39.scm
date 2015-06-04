e2ea2d85bcad2c92d120e94fcfef347fbe97d302
fubupc <fubupc@gmail.com>
2015-06-04 14:09:31 +0800

    2.39

---
+++ chapter2/2.39.scm
@@ -0,0 +1,21 @@
+(define (reverse seq)
+  (fold-left (lambda (result item) (cons item result)) '() seq))
+
+(define (reverse seq)
+  (fold-right (lambda (item rest) (append rest item)) '() seq))
+
+
+(define (fold-left op initial seq)
+  (define (iter result rest)
+    (if (null? rest)
+      result
+      (iter (op result (car rest))
+            (cdr rest))))
+  (iter initial seq))
+
+(define (fold-right op initial seq)
+  (if (?null seq)
+    initial
+    (op (car seq)
+        (fold-right (cdr seq)))))
+
