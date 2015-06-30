3d14bbb042af4a40424574d260dfbf30a012c0d6
fubupc <fubupc@gmail.com>
2015-06-30 19:18:51 +0800

    fix 2.68

--- chapter2/2.68.scm
+++ chapter2/2.68.scm
@@ -1,14 +1,20 @@
 ; leaf: ('leaf symbol weight)
 ; tree: (left-tree right-tree symbols weight)
+
+(define (element-of-set? symb seq)
+  (if (null? seq)
+    false
+    (if (eq? symb (car seq))
+      true
+      (element-of-set? symb (cdr seq)))))
+
 (define (encode-symbol symb tree)
   (if (leaf? tree)
-    (cond ((eq? (leaf-symbol tree) symb)
-    (if (eq? (leaf-symbol tree) symb) 0 1)
+    '()
     (let ((l-tree (left-tree tree))
           (r-tree (right-tree tree)))
-      (let ((left-symbols (tree-symbols l-tree)))
+      (let ((left-symbols (symbols l-tree)))
         (if (element-of-set? symb left-symbols)
           (cons 0 (encode-symbol symb l-tree))
           (cons 1 (encode-symbol symb r-tree)))))))
-
 
--- chapter2/huffman-encoding.scm
+++ chapter2/huffman-encoding.scm
@@ -18,3 +18,74 @@
 
 (define (left-branch tree) (car tree))
 (define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+    (list (symbol-leaf tree))
+    (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+    (weight-leaf tree)
+    (cadddr tree)))
+
+(define (left-tree tree)
+  (if (leaf? tree)
+    (error "leaf node has no left-tree")
+    (car tree)))
+
+(define (right-tree tree)
+  (if (leaf? tree)
+    (error "leaf node has no right-tree")
+    (cadr tree)))
+
+(define (order-pairs pairs)
+  (define (weight-pair pair) (cadr pair))
+
+  (define (min-iter min-pair result rest)
+    (if (null? rest)
+      (list min-pair result)
+      (let ((next (car rest)))
+        (cond ((null? min-pair) (min-iter next result (cdr rest)))
+              ((< (weight-pair next) (weight-pair min-pair))
+               (min-iter next (cons min-pair result) (cdr rest)))
+              (else (min-iter min-pair (cons next result) (cdr rest)))))))
+
+  (if (null? pairs)
+    '()
+    (let ((result (min-iter '() '() pairs)))
+      (let ((min-pair (car result))
+            (rest (cadr result)))
+        (cons min-pair (order-pairs rest))))))
+
+
+(define (pairs->leaf-list pairs)
+  (if (null? pairs)
+    '()
+    (let ((next (car pairs)))
+      (cons (make-leaf (car next) (cadr next))
+            (pairs->leaf-list (cdr pairs))))))
+
+(define (insert-tree tree ordered-list)
+  (if (null? ordered-list)
+    (list tree)
+    (let ((next (car ordered-list)))
+      (if (< (weight tree) (weight next))
+        (cons tree ordered-list)
+        (cons next (insert-tree tree (cdr ordered-list)))))))
+
+(define (generate-huffman-tree pairs)
+  (define (iter tree-list)
+    (let ((len (length tree-list)))
+      (cond ((= len 0) '())
+            ((= len 1) (car tree-list))
+            (else
+              (let ((first (car tree-list))
+                    (second (cadr tree-list))
+                    (rest (cddr tree-list)))
+                (iter (insert-tree (make-code-tree first second) rest)))))))
+
+  (let ((ordered-pairs (order-pairs pairs)))
+    (let ((leaf-list (pairs->leaf-list ordered-pairs)))
+      (iter leaf-list))))
+
