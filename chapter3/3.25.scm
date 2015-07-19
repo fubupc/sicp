66c3c8acb82fab41c286848b2bc197d48d26916b
fubupc <fubupc@gmail.com>
2015-07-19 12:29:00 +0800

    3.25

--- chapter3/3.25.scm
+++ chapter3/3.25.scm
@@ -10,16 +10,41 @@
         (else false)))
 
 
-(define (assoc key table)
-  (cond ((null? table) false)
-        ((equal? key (caar table))
-         (car table))
-        ((else
-           (assoc key (cdr table))))))
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((equal? key (caar records))
+         (car records))
+        (else
+          (assoc key (cdr records)))))
 
 
 (define (lookup keys table)
   (define (lookup-1 keys table)
     (if (null? keys)
       table
-  0)
+      (let ((subtable (assoc (car keys) table)))
+        (if subtable
+          (lookup-1 (cdr keys) (cdr subtable))
+          false))))
+
+  (lookup-1 keys (cdr table)))
+
+(define (insert keys value table)
+  (cond ((null? keys) (error "keys empty"))
+        ((= (length keys) 1)
+         (let ((subtable (assoc (car keys) (cdr table))))
+           (if subtable
+             (set-cdr! subtable value)
+             (set-cdr! table (cons (cons (car keys) value)
+                                   (cdr table))))))
+        (else
+          (let ((subtable (assoc (car keys) (cdr table))))
+            (if subtable
+              (insert (cdr keys) value subtable)
+              (let ((new-table (cons (car keys) '()))) 
+                (set-cdr! table (cons new-table (cdr table)))
+                (insert (cdr keys) value new-table)))))))
+
+
+
+
