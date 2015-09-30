5bc05d3fc9ba2048b81581f2af633b34a69cca03
fubupc <fubupc@gmail.com>
2015-09-30 16:25:04 +0800

    3.31

---
+++ chapter3/3.31.scm
@@ -0,0 +1,4 @@
+;; at first. all wire would be zero.
+;; If input is changed to 1 and 0. The output of first and-gate 
+;; in half-adder would be still 0. so inverter would be still 0.
+;; so sum would be still 0 which is wrong.
